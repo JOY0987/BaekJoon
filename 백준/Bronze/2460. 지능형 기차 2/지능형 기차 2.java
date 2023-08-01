5559fcbfa2163f3de9afe586238a78a4f2aef7a3
Yewon <126743237+JOY0987@users.noreply.github.com>
2023-08-01 21:18:42 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 284 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int human = 0;
+        int max = 0;
+        for (int i = 0; i < 10; i++) {
+            int out = sc.nextInt();
+            int in = sc.nextInt();
+            human -= out;
+            human += in;
+            if (human>max) max = human;
+        }
+        System.out.println(max);
+
+    }
+}
