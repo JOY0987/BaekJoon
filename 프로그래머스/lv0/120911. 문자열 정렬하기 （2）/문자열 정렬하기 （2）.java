44a51bc84f3908b8ff5ae99e30096fdc45bd2457
Yewon <jjoyewon@naver.com>
2023-09-21 03:03:56 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.38 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        
+        char[] strings = my_string
+            .toLowerCase()
+            .toCharArray();
+        Arrays.sort(strings);
+        
+        my_string = String.valueOf(strings);
+        
+        return my_string;
+    }
+}
